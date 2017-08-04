09451f4962c2ede79d783b93948f20306098a6a9
Eric Fletcher <eric.fletcher@noaa.gov>
2017-08-03 14:50:04 -1000

    Add calibr.R for Package Documentation and Package Imports

---
+++ R/calibr.R
@@ -0,0 +1,11 @@
+#' Calibr: A tool to standardize Coral Reef Species population aboundance.
+#'
+#' Calibr is a R-Package designed to process highly diverse coral reef species datasets and to
+#' analyse with Generalized Linear Models (GLM) to obtian standarization factors.
+#'
+#' @import methods
+#' @importFrom utils read.csv
+#'
+#' @docType package
+#' @name Calibr
+NULL
