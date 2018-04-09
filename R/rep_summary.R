767e02e451d4978df5fdb0c01b611ffa3fbb193c
Eric Fletcher <eric.fletcher@noaa.gov>
2018-04-09 10:58:02 -1000

    Fixed unassigned lblock value (part of PIFSCstockassessments/Calibr#1)
    Signed-off-by: Eric Fletcher <eric.fletcher@noaa.gov>

--- R/rep_summary.R
+++ R/rep_summary.R
@@ -40,7 +40,8 @@ rep_summary <- function(SET, std_method) {
   #Make a vector of all BLOCKs where the GROUP was seen at least once by both METHODs.
   #require(plyr)
   #lblock <- plyr::daply(SET, "BLOCK", function(X, m){ unique(X["METHOD"]) %in% m } , m=unique(SET["METHOD"]))
-  plyr::daply(SET, "BLOCK", function(X, m){ unique(X["METHOD"]) %in% m } , m=unique(SET["METHOD"]))
+  lblock <- plyr::daply(SET, "BLOCK", function(X, m){
+    plyr::arrange(unique(X["METHOD"]),METHOD) %in% m } , m=plyr::arrange(unique(SET["METHOD"]),METHOD))
 
   lblock <- names(lblock)[lblock & complete.cases(lblock)] #complete.cases ~ no NA's
 
