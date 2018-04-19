95166895d759f788cec855e1554a1abad67942ad
Eric Fletcher <eric.fletcher@noaa.gov>
2018-04-19 11:35:43 -1000

    Addeed 'stat_type' param field
    Signed-off-by: Eric Fletcher <eric.fletcher@noaa.gov>

--- R/rep_summary.R
+++ R/rep_summary.R
@@ -3,13 +3,18 @@
 #'
 #' Calculates descriptive statistics for each GROUP
 #'
-#' @inheritParams get_reef_datalist
+#' @param SET Dataset
+#' @param std_method Denotes Survey dataset METHOD string as the Standard METHOD
+#' @param stat_type Type of Generalized Linear Model used
 #'
 #' @importFrom dplyr arrange_
 #' @importFrom plyr daply
 #'
 #' @export
-rep_summary <- function(SET, std_method) {
+rep_summary <- function(SET, std_method, stat_type=c("GLM","GLMM")) {
+
+  #validate value for 'stat_type' param
+  stat_type <- match.arg(stat_type)
 
   if(class(SET) == "list"){
     stop("Parameter SET found as a list object.")
