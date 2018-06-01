7ab2a15f77dfe8fb4020113b248fb03fb2d03d0c
Eric Fletcher <eric.fletcher@noaa.gov>
2018-06-01 13:14:59 -1000

    Return parallelized glm output to proper object name, lgroup_gcf.
    Signed-off-by: Eric Fletcher <eric.fletcher@noaa.gov>

--- R/gcf_glm.R
+++ R/gcf_glm.R
@@ -197,7 +197,7 @@ gcf_glm <- function(SET, std_method, min_obs=10, n_sample=5, do_parallel=TRUE) {
     #     message(unique(list_groupset$GROUP) , ": " , trimws(cond))
     #   }
     # )
-    out_time <- system.time( Out <- pblapply(list_groupset, gcf_glm_apply, std_method=std_method, cl=cl) )
+    out_time <- system.time( lgroup_gcf <- pblapply(list_groupset, gcf_glm_apply, std_method=std_method, cl=cl) )
 
     message("Parallel processing times (in seconds):")
     print(out_time)
