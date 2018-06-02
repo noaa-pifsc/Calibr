bc3973c61f2db9981e83c84c416093232311c379
Eric Fletcher <eric.fletcher@noaa.gov>
2018-06-01 14:09:20 -1000

    Code cleanup: Remove commented code
    Signed-off-by: Eric Fletcher <eric.fletcher@noaa.gov>

--- R/run_calibr.R
+++ R/run_calibr.R
@@ -61,29 +61,6 @@ run_calibr <- function(SET, std_method, stat_model=c("GLM","GLMM"), n_sample=5){
 
   if(stat_model=="GLM"){
 
-    # #Lapply gcf function for all species
-    # lgroup_gcf <- lapply(fish_datalist,function(X){
-    #   message("Group: ", unique(X$GROUP))
-    #   tryCatch(
-    #     if(stat_model=="GLM"){
-    #       gcf_glm(SET=X, std_method=std_method_factorname)
-    #     }
-    #     ,#End of code expresssion
-    #     error=function(cond){
-    #       message(unique(X$GROUP) , ": ", trimws(cond), " Returning NA.")
-    #       return(NA)
-    #     },
-    #     warning=function(cond){
-    #       message(unique(X$GROUP) , ": " , trimws(cond))
-    #     }
-    #   )
-    #
-    # })
-    #
-    # #Remove GROUP objects that have null(NA) data
-    # lgroup_calibrated <- lgroup_gcf[!(is.na(lgroup_gcf))]
-    # #Summary descriptive statistics for each GROUP
-    # gcf_summary <- suppressMessages(Reduce(function(...)merge(...,all=TRUE),lgroup_calibrated))
     gcf_summary <- gcf_glm(SET, std_method = std_method_factorname)
     n_sufficentREPS <- nrow(gcf_summary)
 
